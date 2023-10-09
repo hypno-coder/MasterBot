37b46d544c6af46c0b8e5542d9e5b15ec47f37df
Артур Давидов <arturdavidov@Arturs-MacBook-Air.local>
2023-10-09 18:14:06 +0300

    fix price for paid menu

--- lexicon/paid_menu/buttons.py
+++ lexicon/paid_menu/buttons.py
@@ -1,8 +1,9 @@
 from enum import Enum
+from loader import PaymentCredentials
 
 class PaidMenuButtons(Enum):
-    Jantra = 'Янтра'
-    MoneyCode = 'ФинКод'
-    MoneyCalendar = 'Денежный Календарь'
+    Jantra = f'Янтра: {PaymentCredentials.price.jantra}₽'
+    MoneyCode = f'ФинКод: {PaymentCredentials.price.money_code}₽'
+    MoneyCalendar = f'Денежный Календарь: {PaymentCredentials.price.money_calendar}₽'
     BackToPaidMenu = 'Назад'
 
