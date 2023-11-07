02a999694a35dfa3eabdc4e5e45fd0259dc9060c
Артур Давидов <arturdavidov@Arturs-MacBook-Air.local>
2023-11-07 14:28:31 +0300

    fix complete

--- services/money_code.py
+++ services/money_code.py
@@ -1,14 +1,40 @@
 from errors import send_error_message
 
-async def calculate_code(berthday: str) -> str:
-    day, month, year = map(int, berthday.split('.'))
-    try:
-        day_sum = sum(map(int, str(day)))
-        month_sum = sum(map(int, str(month)))
+class FinCode: 
+
+    async def calculate(self, berthday: str) -> str:
+        self.__day, self.__month, self.__year = map(int, berthday.split('.'))
+        try:
+            day = self.__calc_day(self.__day)
+            month = self.__calc_month(self.__month) 
+            year = self.__calc_year(self.__year) 
+            code = self.__calc_code(day, month, year) 
+            return f"{day}{month}{year}{code}"
+        except Exception as ex:
+            print(f'Ошибка {ex}')
+            await send_error_message("Ошибка подсчета денежного кода")
+            return ("Ошибка подсчета денежного кода")
+
+    def __calc(self, data) -> int:
+        data_sum = sum(map(int, str(data))) 
+        if data_sum > 9:
+            data_sum =  self.__calc(data_sum)
+        return data_sum
+
+    def __calc_day(self, day) -> int:
+        return self.__calc(day) 
+
+    def __calc_month(self, month) -> int:
+        return self.__calc(month) 
+
+    def __calc_year(self, year) -> int:
         year_sum = sum(map(int, str(sum(map(int, str(year))))))
-        code = sum(map(int, str(day_sum + month_sum + year_sum)))
-        return f"{day_sum}{month_sum}{year_sum}{code}"
-    except Exception as ex:
-        print(f'Ошибка {ex}')
-        await send_error_message("Ошибка подсчета денежного кода")
-        return ("Ошибка подсчета денежного кода")
+        if year_sum > 9:
+            year_sum = self.__calc_month(year_sum)
+        return year_sum
+
+    def __calc_code(self, day, month, year) -> int:
+        code_sum = sum(map(int, str(day + month + year)))
+        if code_sum > 9:
+            return self.__calc(code_sum)
+        return code_sum
--- utils/utils.py
+++ utils/utils.py
@@ -8,7 +8,7 @@ from lexicon import CommonLexicon, PaidMenuButtons, CalendarLexicon, CodeLexicon
 from keyboards.keyboards_generator import Keyboard 
 from payment_services.user_data_type import UserDataType
 from staticfiles import FilePath
-from services import get_calendar_dates, calculate_code, Jantra
+from services import get_calendar_dates, FinCode, Jantra
 
 async def remove_message(chat_id: int, message_id: int, delay: int = 60) -> None:
     try:
@@ -57,7 +57,8 @@ async def send_response(
         case PaidMenuButtons.MoneyCode.name:
             code_doc = FSInputFile(FilePath.money_code_pdf.value)
             code_video = FSInputFile(FilePath.money_code_video.value)
-            code_result: str = await calculate_code(birthday) 
+            fincode = FinCode()
+            code_result: str = await fincode.calculate(birthday) 
             await bot.send_message(chat_id=chat_id, text=f'<i>{PaidMenuButtons.MoneyCode.value}</i>')
             await bot.send_document(chat_id, code_doc, caption=CodeLexicon.document)
             await bot.send_message(chat_id, f'<b>{CodeLexicon.for_you}</b> {code_result}')
@@ -79,52 +80,3 @@ async def send_response(
             text=CommonLexicon.back_menu, 
             reply_markup=Keyboard.create_inline(
                 ITEMS_PER_ROW, backButton=PaidMenuButtons.BackToPaidMenu)) 
-
-async def send_message_with_delay(
-        chat_id: int, 
-        name: str,
-        back_button_callback: Enum | None = None,
-        min_delay: int = 1800, 
-        max_delay: int = 4800,
-        greeting: str | None = None,
-        text: str | None = None,
-        image: BufferedInputFile | None = None,
-        video: FSInputFile | None = None,
-        document:  FSInputFile | None = None,
-        document_caption: str = ''
-        ) -> None:
-
-    def get_keyboard():
-        ITEMS_PER_ROW = 1
-        if back_button_callback != None:
-            return Keyboard.create_inline(ITEMS_PER_ROW, backButton=back_button_callback) 
-        return None
-
-    reply = await bot.send_message(
-            chat_id,
-            text=CommonLexicon.pay_success +' '+ CommonLexicon.message_delay + f'{round(min_delay/60)}-{round(max_delay/60)} минут', 
-            reply_markup=get_keyboard())
-
-    delay = random.randint(min_delay, max_delay)
-    await sleep(delay)
-    try:
-        await bot.delete_message(chat_id=reply.chat.id, message_id=reply.message_id)
-    except Exception as ex:
-        print(ex)
-    await bot.send_message(chat_id=chat_id, text='===========================')
-    await bot.send_message(chat_id=chat_id, text=f'<b>{name}</b>')
-
-    if greeting != None:
-        await bot.send_message(chat_id=chat_id, text=f'<i>{greeting}</i>')
-    if image != None:
-        await bot.send_photo(chat_id, image)
-    if text != None:
-        await bot.send_message(chat_id, text)
-    if document != None:
-        await bot.send_document(chat_id, document, caption=document_caption)
-    if video != None:
-        await bot.send_video(chat_id, video)
-
-    await bot.send_message(chat_id=chat_id, text='===========================')
-
-    await bot.send_message(chat_id, text=CommonLexicon.back_menu, reply_markup=get_keyboard()) 
