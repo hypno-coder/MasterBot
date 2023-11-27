64d5870ac96fe0636f5eace8bc9f8e167669a8aa
Артур Давидов <arturdavidov@Arturs-MacBook-Air.local>
2023-11-27 12:58:32 +0300

    mailing is complete

--- services/sender.py
+++ services/sender.py
@@ -1,3 +1,4 @@
+import asyncio
 from aiogram import Bot
 
 from database import User
@@ -8,7 +9,9 @@ from loader import config
 
 class Sender: 
     __error_count = 0
+    __loop_count = 0
     __mailing_count = 0
+    __number_of_attempts = 0
 
     def __init__(self, bot: Bot, data):
         self.bot = bot
@@ -16,55 +19,59 @@ class Sender:
 
     async def start(self):
         users = await User.get_all()
-        loop_count = 0
         try:
-            for user in users[self.__mailing_count:4]:
-                await self.__mailing(user)
-                loop_count += 1
-                redis_db.setex('mailing_count', 86400, loop_count)
-        except:
-            self.__error_count += 1
-            if self.__error_count == 5:
-                await send_error_message(f'Ошибка рассылки! Разослали {self.__mailing_count} / {len(users)+1}')
-                return
-
-            count_bytes = redis_db.get('mailing_count')
-            count = int(count_bytes.decode('utf-8')) if count_bytes else 0
-
-            if count is not None:
-                # +2 потому что список с 0 начинается и рассылку нужно начать со следующего пользователя
-                self.__mailing_count = int(count) + 2
-
-            await self.start()
-
+            await self.__mailing_loop(users)
+        except Exception:
+            print(f'Ошибка рассылки (попытка {self.__error_count})')
+            await self.__error_action(users)
         else:
-            count_bytes = redis_db.get('mailing_count')
-            count = int(count_bytes.decode('utf-8')) if count_bytes else 0
-            for admin_id in config.tg_bot.admin_ids:
-                await self.bot.send_message(
-                    chat_id=admin_id, 
-                    text=f'Рассылка завершена! Разослали {count} / {len(users)+1}')
+            await self.__mailing_complete_action(users)
+            
+
+    async def __mailing_complete_action(self, users):
+        count = self.__get_mailing_count()
+        for admin_id in config.tg_bot.admin_ids:
+            await self.bot.send_message(
+                chat_id=admin_id, 
+                text=f'Рассылка завершена! Разослали {count} / {len(users)+1}')
+
+
+    async def __error_action(self, users):
+        DELAY_BEFORE_REPEATING = 3
+        self.__error_count += 1
+        count = self.__get_mailing_count()
+        if count is not None:
+            self.__mailing_count = int(count)
+        
+        if self.__error_count < self.__number_of_attempts:
+            await asyncio.sleep(DELAY_BEFORE_REPEATING)
+            return await self.start()
+        else:
+            await send_error_message(f'Ошибка рассылки! Разослали {self.__mailing_count} / {len(users)+1}')
+
+
+    async def __mailing_loop(self, users):
+        for user in users[self.__mailing_count:]:
+            self.__loop_count += 1
+            await self.__mailing(user)
+            redis_db.setex('mailing_count', 86400, self.__loop_count)
 
 
     async def __mailing(self, user):
         await self.bot.send_message(chat_id=user['_id'], text=f'Приветствую {user["username"]}!')
         if self.mailing_data['photo_id'] != '0':
             await self.bot.send_photo(user['_id'], self.mailing_data['photo_id'])
-        await self.bot.send_message(chat_id=user['_id'], text=self.mailing_data["mailing_message"])
+        if self.mailing_data['button_name'] is None:
+            await self.bot.send_message(chat_id=user['_id'], text=self.mailing_data['mailing_message'])
         if self.mailing_data['button_name'] is not None:
             await self.bot.send_message(
                     chat_id=user['_id'], 
-                    text=self.mailing_data["mailing_message"],
+                    text=self.mailing_data['mailing_message'],
                     reply_markup=get_mailing_button(
                         self.mailing_data['button_name'], 
                         self.mailing_data['button_link']))
 
 
-    def __save_mailing_state(self):
-        pass
-
-
-
-    
-
-       
+    def __get_mailing_count(self):
+        count_bytes = redis_db.get('mailing_count')
+        return int(count_bytes.decode('utf-8')) if count_bytes else 0
