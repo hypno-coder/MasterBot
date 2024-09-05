633b89f2b8a42abda2bddfc7466240d3e57bbb28
Hypnocoder <davidov.artur.job@gmail.com>
2024-09-05 18:21:42 +0300

    goroskop is complete

--- url_const.py
+++ url_const.py
@@ -5,6 +5,6 @@ WEBHOOK_PATH = f"/bot/{config.tg_bot.token}"
 WEBHOOK_URL = f"{config.tg_bot.base_url}{WEBHOOK_PATH}"
 
 # Dev
-WEBHOOK_NGROK = "https://1664-85-174-193-139.ngrok-free.app"
+WEBHOOK_NGROK = "https://2d80-83-243-121-5.ngrok-free.app"
 WEBHOOK_PATH_DEV = f"/bot/6698538164:AAHKmaLNV_IHegbLu5o_y8HDkEpq77xfhu0"
 WEBHOOK_URL_DEV = f"{WEBHOOK_NGROK}{WEBHOOK_PATH_DEV}"
