ece54dcda4d143e2f2c1e4bef61a6eb52e64a926
Artur Davidov <davidov.artur.job@gmail.com>
2024-10-21 15:26:52 +0300

    Remove sbornik (#22)
    * remove sbornik
    
    * remove sbornik +

--- lexicon/main_menu/buttons.py
+++ lexicon/main_menu/buttons.py
@@ -2,7 +2,6 @@ from enum import Enum
 
 class MainMenuButtons(Enum):
     BackToMainMenu = 'Главное Меню'
-    MoenyCollection = 'Денежный сборник'
     PaidServices = 'Платные Услуги 💰'
     FreeServices = 'Бесплатные Услуги 🆓'
 
