9699d1a7f74d9eb2aa1ac14f91939223100cc261
Hypnocoder <davidov.artur.job@gmail.com>
2025-11-03 13:24:49 +0300

    signature verify refatoring to OOP

--- payment_services/prodamus/__init__.py
+++
@@ -1,1 +0,0 @@
-from link_generator import ProdamusPayment
--- payment_services/prodamus/test_link.py
+++ payment_services/prodamus/test_link.py
@@ -1,13 +1,11 @@
 import hashlib
 import hmac
 import json
-
-# from typing import Literal
 from collections.abc import MutableMapping
+from typing import Literal
 from urllib.parse import urlencode
 
 # from loader import payment 
-
 
 class ProdamusClient:
     # linktoform = payment.prodamus.link_to_form
@@ -15,7 +13,7 @@ class ProdamusClient:
     linktoform = 'https://christina-astra.payform.ru/'
     secret_key = '657fc2074e897d8f8ac806e0583b30ecbd0f79b64b5e1b213508b68540427e9c'
     
-    def generate_link(self):
+    def generate_link(self) -> str:
 
         data = {
             'order_id': "123556",
@@ -35,40 +33,42 @@ class ProdamusClient:
             'callbackType': 'json'
         }
 
-        signature = self._sign(data)
+        signature = self.make_signature(data, "link")
         data['signature'] = signature
         link = self.linktoform + '?' + urlencode(self._http_build_query(data))
         return link
 
+    def make_signature(self, data: dict, mode: Literal["link", "webhook"]) -> str:
+        match mode:
+            case "link":
+                self._deep_int_to_string(data)
+                payload = json.dumps(data, sort_keys=True, ensure_ascii=False, separators=(",", ":"))
+            case "webhook":
+                clean = self._deep_str_and_sort(data)
+                payload = json.dumps(clean, ensure_ascii=False, separators=(",", ":"))
+            case _:
+                raise ValueError("Invalid mode") 
+
+        payload = payload.replace("/", "\\/")
+        return hmac.new(self.secret_key.encode("utf8"), payload.encode("utf8"), hashlib.sha256).hexdigest()
     
-    # def make_signature(self, data: dict, secret: str) -> str:
-    #     """Алгоритм подписи из Hmac.php / Hmac.js Prodamus"""
+    def _deep_int_to_string(self, d):
+        for k, v in d.items():
+            if isinstance(v, MutableMapping):
+                self._deep_int_to_string(v)
+            elif isinstance(v, (list, tuple)):
+                for i, x in enumerate(v):
+                    self._deep_int_to_string({str(i): x})
+            else:
+                d[k] = str(v)
 
-    #     def deep_str_and_sort(d):
-    #         if isinstance(d, dict):
-    #             return {k: deep_str_and_sort(d[k]) for k in sorted(d.keys())}
-    #         if isinstance(d, list):
-    #             return [deep_str_and_sort(x) for x in d]
-    #         return str(d)
-
-    #     clean = deep_str_and_sort(data)
-    #     json_str = json.dumps(clean, ensure_ascii=False, separators=(',', ':')).replace('/', '\\/')
-    #     return hmac.new(secret.encode(), json_str.encode(), hashlib.sha256).hexdigest()
-
-    def _sign(self, data):
-
-        self._deep_int_to_string(data)
-        data_json = json.dumps(data, sort_keys=True, ensure_ascii=False, separators=(',', ':')).replace("/", "\\/")
-        return hmac.new(self.secret_key.encode('utf8'), data_json.encode('utf8'), hashlib.sha256).hexdigest()
-
-    def _deep_int_to_string(self, dictionary):
-        for key, value in dictionary.items():
-            if isinstance(value, MutableMapping):
-                self._deep_int_to_string(value)
-            elif isinstance(value, list) or isinstance(value, tuple):
-                for k, v in enumerate(value):
-                    self._deep_int_to_string({str(k): v})
-            else: dictionary[key] = str(value)
+    def _deep_str_and_sort(self, d):
+        if isinstance(d, dict):
+            return {k: self._deep_str_and_sort(d[k]) for k in sorted(d.keys())}
+        if isinstance(d, list):
+            return [self._deep_str_and_sort(x) for x in d]
+        return str(d)
+        
 
     def _http_build_query(self, dictionary, parent_key=False):
         items = []
